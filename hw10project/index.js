337ae9fb86a48e7586726cc0fb6f1292a318c16d
ErfanGh <erfan.ghasemian83@gmail.com>
2024-10-29 10:33:41 +0330

    contact info section almost completed (delete contact function remains)

--- hw10project/index.js
+++ hw10project/index.js
@@ -1,12 +1,14 @@
 let contactsDB = [];
 
 const showCreateBtn = document.querySelector(".show-create-button");
-const backFromCreateBtn = document.querySelector(".back-from-create-button")
-const submitNewContact = document.querySelector(".submit-new-contact-button")
+const backFromCreateBtn = document.querySelector(".back-from-create-button");
+const submitNewContact = document.querySelector(".submit-new-contact-button");
+const closeContactInfoBtn = document.querySelector(".close-contact-info-button");
 
-let createContactBlock = document.querySelector(".create-contact-block")
-let mainBlock = document.querySelector(".container")
-let header = document.querySelector(".header")
+let createContactBlock = document.querySelector(".create-contact-block");
+let mainBlock = document.querySelector(".container");
+let header = document.querySelector(".header");
+let contactInfoBlock = document.querySelector(".contact-info-block");
 
 let firstNameInput = document.querySelector(".fname-input__text");
 let phoneNumberInput = document.querySelector(".phone-input__text");
@@ -14,33 +16,34 @@ let phoneNumberInput = document.querySelector(".phone-input__text");
 showCreateBtn.addEventListener('click', showCreateContactBlock);
 backFromCreateBtn.addEventListener('click', showContainer);
 submitNewContact.addEventListener('click', submitContact);
+closeContactInfoBtn.addEventListener('click', closeContactInfo);
 
 function submitContact(){
     if(firstNameInput.value && phoneNumberInput.value){
-        createContact(firstNameInput.value,"", phoneNumberInput.value)
-        firstNameInput.value = ""
-        phoneNumberInput.value = ""
+        createContact(firstNameInput.value,"", phoneNumberInput.value);
+        firstNameInput.value = "";
+        phoneNumberInput.value = "";
     }
 }
 
 function randomColor() {
     let colorArr = ['bg-red-400','bg-blue-400','bg-orange-400','bg-green-400','bg-yellow-400','bg-purple-400', 'bg-pink-400', 'bg-violet-400'];
 
-    return colorArr[(Math.floor(Math.random() * colorArr.length))]
+    return colorArr[(Math.floor(Math.random() * colorArr.length))];
 }
 
 randomColor()
 
 function showCreateContactBlock(){
-    createContactBlock.classList.remove('hidden')
-    mainBlock.classList.add('hidden')
-    header.classList.add('hidden')
+    createContactBlock.classList.remove('hidden');
+    mainBlock.classList.add('hidden');
+    header.classList.add('hidden');
 }
 
 function showContainer(){
-    mainBlock.classList.remove('hidden')
-    header.classList.remove('hidden')
-    createContactBlock.classList.add('hidden')
+    mainBlock.classList.remove('hidden');
+    header.classList.remove('hidden');
+    createContactBlock.classList.add('hidden');
 }
 
 function createContact(fname, lname, pnum){
@@ -103,19 +106,33 @@ function displayContacts(){
             nameCell.innerHTML = `<div class="${randomColor()} w-6 h-6 rounded-full inline-block mx-2 my-2"><h1 class="mt-[-3px] ml-2">${fname.slice(0,1)}</h1></div><h1 class="inline w-4">${fname + " " + lname}</h1>`;
             phoneCell.innerHTML = `<h1 class="w-1/2 ">${pnum}</h1>`;
 
+            row.classList.add(`${fname}-${pnum}`)
             row.classList.add('cursor-pointer')
             row.classList.add('hover:bg-gray-50')
 
-            row.addEventListener('mouseover', ()=>(showCallButton(this)));
+            row.addEventListener('click', ()=>(showContactInfo(row.classList)));
         }
     }
 }
 
-function showCallButton(row){
-    row.innerHTML += "<button class="+row+">Call</button>"
+function showContactInfo(rowClasses){
+    mainBlock.classList.add('hidden');
+    header.classList.add('hidden');
+    contactInfoBlock.classList.remove('hidden');
+
+    let fnameLabel = document.querySelector('.full-name__text');
+
+    let name_pnum = rowClasses[0].split('-');
+    let fname = name_pnum[0];
+    let pnum = name_pnum[1];
+
+    fnameLabel.innerText = `${fname} \n ${pnum}`;
 }
 
-// function hideCallButton(row){
-//     row.inn
-// }
+function closeContactInfo(){
+    mainBlock.classList.remove('hidden');
+    header.classList.remove('hidden');
+    contactInfoBlock.classList.add('hidden');
+}
+
 displayContacts();
