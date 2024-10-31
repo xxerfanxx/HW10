05c58092ffae301a4ea27cb6a0ea83567351b426
ErfanGh <erfan.ghasemian83@gmail.com>
2024-10-31 13:12:42 +0330

    added transfering option to task cards

--- hw10project2/index.js
+++ hw10project2/index.js
@@ -99,19 +99,24 @@ function displayCards(){
                     <p class="task__details w-full max-w-96 break-words">${database.to_do[i].description}</p>
                   </div>
 
-                  <div class="task__tags">
-                    <ul class="tags flex flex-row w-full my-2">
-                        ${tagsHTML}
-                    </ul>
-                  </div>
-
-                  <div class="task__footer flex flex-row justify-between mt-2">
-                    <h3 class="task__doer text-xs max-w-36 whitespace-nowrap overflow-ellipsis overflow-hidden">${database.to_do[i].taskDoer}</h3>
-                    <div class="task__dates flex flex-row">
-                        <h3 class="text-xs">created:</h3><h3 class="task__creation-date text-xs mr-10 max-w-16 whitespace-nowrap overflow-ellipsis overflow-hidden">${database.to_do[i].creationDate}</h3>
-                        <h3 class="text-xs">due:</h3><h3 class="task__due-date text-xs max-w-16 whitespace-nowrap overflow-ellipsis overflow-hidden">${database.to_do[i].dueDate}</h3>
+                    <div class="task__tags">
+                        <ul class="tags flex flex-row w-full my-2">
+                            ${tagsHTML}
+                        </ul>
                     </div>
-                  </div>
+
+                    <div class="task__footer flex flex-row justify-between mt-2">
+                        <h3 class="task__doer text-xs max-w-36 whitespace-nowrap overflow-ellipsis overflow-hidden">${database.to_do[i].taskDoer}</h3>
+                        <div class="task__dates flex flex-row">
+                            <h3 class="text-xs">created:</h3><h3 class="task__creation-date text-xs mr-10 max-w-16 whitespace-nowrap overflow-ellipsis overflow-hidden">${database.to_do[i].creationDate}</h3>
+                            <h3 class="text-xs">due:</h3><h3 class="task__due-date text-xs max-w-16 whitespace-nowrap overflow-ellipsis overflow-hidden">${database.to_do[i].dueDate}</h3>
+                        </div>
+                    </div>
+                    <div class="my-2 w-full flex flex-row-reverse"> 
+                        <button class="transfer-next__button w-26 h-6 flex flex-row justify-between" onclick="transferTaskNext(${database.to_do[i].id},'to_do')">
+                            <h1 class="text-xs">transfer task</h1><img class="transfer-next__img w-4 h-4" src="./Assets/next-icon.png">
+                        </button>
+                    </div>
                 </div>
               </li>`
     }
@@ -125,29 +130,43 @@ function displayCards(){
         
         doingTasksContainer.innerHTML += `<li class="task-id-${database.doing[i].id}">
                 <div class="task w-96 ${database.doing[i].color} rounded-md my-4 p-2 flex flex-col shadow-lg">
-                  <div class="top-row flex flex-row justify-between">
-                    <button class="edit-task__button w-6 h-6" onclick="editTask(${database.doing[i].id},'doing')"><img class="w-6 h-6" src="./Assets/edit-icon.png"></button>
-                    <h1 class="task__title text-center text-2xl max-w-36 whitespace-nowrap overflow-ellipsis overflow-hidden">${database.doing[i].title}</h1>
-                    <button class="delete-task__button w-6 h-6" onclick="deleteTask(${database.doing[i].id},'doing')"><img class="w-6 h-6" src="./Assets/delete-icon.png"></button>
-                  </div>
-
-                  <div class="middle-row w-full my-4 bg-white rounded-sm h-fit shadow-inner">
-                    <p class="task__details w-full max-w-96 break-words">${database.doing[i].description}</p>
-                  </div>
-
-                  <div class="task__tags">
-                    <ul class="tags flex flex-row w-full my-2">
-                        ${tagsHTML}
-                    </ul>
-                  </div>
-
-                  <div class="task__footer flex flex-row justify-between mt-2">
-                    <h3 class="task__doer text-xs max-w-36 whitespace-nowrap overflow-ellipsis overflow-hidden">${database.doing[i].taskDoer}</h3>
-                    <div class="task__dates flex flex-row">
-                        <h3 class="text-xs">created:</h3><h3 class="task__creation-date text-xs mr-10 max-w-16 whitespace-nowrap overflow-ellipsis overflow-hidden">${database.doing[i].creationDate}</h3>
-                        <h3 class="text-xs">due:</h3><h3 class="task__due-date text-xs max-w-16 whitespace-nowrap overflow-ellipsis overflow-hidden">${database.doing[i].dueDate}</h3>
+                    <div class="top-row flex flex-row justify-between">
+                        <button class="edit-task__button w-6 h-6" onclick="editTask(${database.doing[i].id},'doing')"><img class="w-6 h-6" src="./Assets/edit-icon.png"></button>
+                        <h1 class="task__title text-center text-2xl max-w-36 whitespace-nowrap overflow-ellipsis overflow-hidden">${database.doing[i].title}</h1>
+                        <button class="delete-task__button w-6 h-6" onclick="deleteTask(${database.doing[i].id},'doing')"><img class="w-6 h-6" src="./Assets/delete-icon.png"></button>
                     </div>
-                  </div>
+
+                    <div class="middle-row w-full my-4 bg-white rounded-sm h-fit shadow-inner">
+                        <p class="task__details w-full max-w-96 break-words">${database.doing[i].description}</p>
+                    </div>
+
+                    <div class="task__tags">
+                        <ul class="tags flex flex-row w-full my-2">
+                            ${tagsHTML}
+                        </ul>
+                    </div>
+
+                    <div class="task__footer flex flex-row justify-between mt-2">
+                        <h3 class="task__doer text-xs max-w-36 whitespace-nowrap overflow-ellipsis overflow-hidden">${database.doing[i].taskDoer}</h3>
+                        <div class="task__dates flex flex-row">
+                            <h3 class="text-xs">created:</h3><h3 class="task__creation-date text-xs mr-10 max-w-16 whitespace-nowrap overflow-ellipsis overflow-hidden">${database.doing[i].creationDate}</h3>
+                            <h3 class="text-xs">due:</h3><h3 class="task__due-date text-xs max-w-16 whitespace-nowrap overflow-ellipsis overflow-hidden">${database.doing[i].dueDate}</h3>
+                        </div>
+                    </div>
+
+                    <div class="transfer flex flex-row-reverse mt-2">
+                        <div class="my-2 w-full flex flex-row-reverse"> 
+                            <button class="transfer-next__button w-26 h-6 flex flex-row justify-between" onclick="transferTaskNext(${database.doing[i].id},'doing')">
+                                <h1 class="text-xs">transfer task (next)</h1><img class="transfer-next__img w-4 h-4" src="./Assets/next-icon.png">
+                            </button>
+                        </div>
+
+                        <div class="my-2 w-full flex flex-row"> 
+                            <button class="transfer-next__button w-26 h-6 flex flex-row- justify-between" onclick="transferTaskBack(${database.doing[i].id},'doing')">
+                                <img class="transfer-next__img w-[20px] h-[20px]" src="./Assets/back-icon.png"><h1 class="text-xs">transfer task (back)</h1>
+                            </button>
+                        </div>
+                    </div>
                 </div>
               </li>`
     }
@@ -161,31 +180,94 @@ function displayCards(){
         
         doneTasksContainer.innerHTML += `<li class="task-id-${database.done[i].id}">
                 <div class="task w-96 ${database.done[i].color} rounded-md my-4 p-2 flex flex-col shadow-lg">
-                  <div class="top-row flex flex-row justify-between">
-                    <button class="edit-task__button w-6 h-6" onclick="editTask(${database.done[i].id},'done')"><img class="w-6 h-6" src="./Assets/edit-icon.png"></button>
-                    <h1 class="task__title text-center text-2xl max-w-36 whitespace-nowrap overflow-ellipsis overflow-hidden">${database.done[i].title}</h1>
-                    <button class="delete-task__button w-6 h-6" onclick="deleteTask(${database.done[i].id},'done')"><img class="w-6 h-6" src="./Assets/delete-icon.png"></button>
-                  </div>
-
-                  <div class="middle-row w-full my-4 bg-white rounded-sm h-fit shadow-inner">
-                    <p class="task__details w-full max-w-96 break-words">${database.done[i].description}</p>
-                  </div>
-
-                  <div class="task__tags">
-                    <ul class="tags flex flex-row w-full my-2">
-                        ${tagsHTML}
-                    </ul>
-                  </div>
-
-                  <div class="task__footer flex flex-row justify-between mt-2">
-                    <h3 class="task__doer text-xs max-w-36 whitespace-nowrap overflow-ellipsis overflow-hidden">${database.done[i].taskDoer}</h3>
-                    <div class="task__dates flex flex-row">
-                        <h3 class="text-xs">created:</h3><h3 class="task__creation-date text-xs mr-10 max-w-16 whitespace-nowrap overflow-ellipsis overflow-hidden">${database.done[i].creationDate}</h3>
-                        <h3 class="text-xs">due:</h3><h3 class="task__due-date text-xs max-w-16 whitespace-nowrap overflow-ellipsis overflow-hidden">${database.done[i].dueDate}</h3>
+                    <div class="top-row flex flex-row justify-between">
+                        <button class="edit-task__button w-6 h-6" onclick="editTask(${database.done[i].id},'done')"><img class="w-6 h-6" src="./Assets/edit-icon.png"></button>
+                        <h1 class="task__title text-center text-2xl max-w-36 whitespace-nowrap overflow-ellipsis overflow-hidden">${database.done[i].title}</h1>
+                        <button class="delete-task__button w-6 h-6" onclick="deleteTask(${database.done[i].id},'done')"><img class="w-6 h-6" src="./Assets/delete-icon.png"></button>
                     </div>
-                  </div>
+
+                    <div class="middle-row w-full my-4 bg-white rounded-sm h-fit shadow-inner">
+                        <p class="task__details w-full max-w-96 break-words">${database.done[i].description}</p>
+                    </div>
+
+                    <div class="task__tags">
+                        <ul class="tags flex flex-row w-full my-2">
+                            ${tagsHTML}
+                        </ul>
+                    </div>
+
+                    <div class="task__footer flex flex-row justify-between mt-2">
+                        <h3 class="task__doer text-xs max-w-36 whitespace-nowrap overflow-ellipsis overflow-hidden">${database.done[i].taskDoer}</h3>
+                        <div class="task__dates flex flex-row">
+                            <h3 class="text-xs">created:</h3><h3 class="task__creation-date text-xs mr-10 max-w-16 whitespace-nowrap overflow-ellipsis overflow-hidden">${database.done[i].creationDate}</h3>
+                            <h3 class="text-xs">due:</h3><h3 class="task__due-date text-xs max-w-16 whitespace-nowrap overflow-ellipsis overflow-hidden">${database.done[i].dueDate}</h3>
+                        </div>
+                    </div>
+
+                    <div class="my-2 w-full flex flex-row"> 
+                        <button class="transfer-next__button w-26 h-6 flex flex-row- justify-between" onclick="transferTaskBack(${database.done[i].id},'done')">
+                            <img class="transfer-next__img w-[20px] h-[20px]" src="./Assets/back-icon.png"><h1 class="text-xs">transfer task (back)</h1>
+                        </button>
+                    </div>
                 </div>
               </li>`
+    }
+}
+
+function transferTaskNext(id,type){
+    let selectedTask = document.querySelector(`.task-id-${id}`);
+
+    switch(type){
+        case 'to_do':
+            for(i = 0; i < database.to_do.length; i++){
+                if(database.to_do[i].id == id){
+                    database.doing.push(database.to_do[i]);
+                    deleteTask(id,type);
+                    displayCards()
+                    break;
+                }
+            }
+            break;
+
+        case 'doing':
+            for(i = 0; i < database.doing.length; i++){
+                if(database.doing[i].id == id){
+                    database.done.push(database.doing[i]);
+                    deleteTask(id,type);
+                    displayCards()
+                    break;
+                }
+            }
+            break;
+    }
+    console.log(database)
+}
+
+function transferTaskBack(id,type){
+    let selectedTask = document.querySelector(`.task-id-${id}`);
+
+    switch(type){
+        case 'doing':
+            for(i = 0; i < database.doing.length; i++){
+                if(database.doing[i].id == id){
+                    database.to_do.push(database.doing[i]);
+                    deleteTask(id,type);
+                    displayCards()
+                    break;
+                }
+            }
+            break;
+
+        case 'done':
+            for(i = 0; i < database.done.length; i++){
+                if(database.done[i].id == id){
+                    database.doing.push(database.done[i]);
+                    deleteTask(id,type);
+                    displayCards()
+                    break;
+                }
+            }
+            break;
     }
 }
 
